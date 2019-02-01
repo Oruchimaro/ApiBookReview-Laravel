7bfdc6cc7b09e3e56177b657e973f44ee3d6a658
Itachi Uchiha <itachi@itachi-laptpop-centos.lan>
2019-02-01 14:38:12 +0330

    Adding user authentication using 'tymon/jwt-auth' and configuring the package,adding needed methods to User model needed for the package,configuring 'config/auth.php' file for auth guards to use jwt guard,and making 'AuthController' for later use.

--- app/User.php
+++ app/User.php
@@ -4,9 +4,10 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Tymon\JWTAuth\Contracts\JWTSubject; //implementing JWTauth
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject 
 {
     use Notifiable;
 
@@ -31,4 +32,23 @@ class User extends Authenticatable
     {
         return $this->hasMany('Book::class');
     }
+
+    /**==================================JWT=========================================== */
+    //JWTauth Methods that we need so we can use jwtauthentication
+    /** This gets the identifier that will be stored in
+     *  the subject claim of the JWT
+    */
+    public function getJWTIdentifier()
+    {
+      return $this->getKey();
+    }
+
+    /**
+     *  This allow us to add any custom claims we want added to the JWT
+     */
+    public function getJWTCustomClaims()
+    {
+      return [];
+    }
+    /**=================================JWT End=========================================== */
 }
